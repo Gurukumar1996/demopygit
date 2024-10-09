a048f9f7279727e4506af3c46b46ac0406c4379d
Gurukumar <gurukumar.jangam1996@gmail.com>
2024-10-10 00:22:46 +0530

    One program added

--- demo.py
+++ demo.py
@@ -1,1 +1,15 @@
 print("Hello")
+
+#Factorial Numbers
+
+
+def fact(n):
+    f = 1
+    for i in range(1,n+1):         # 1*2*3*4*5
+        f = f*i
+    
+    return f
+
+x = 5
+result = fact(x)
+print(result)
